ad5608c09945a771f7c0dda1f889b5e75c766998
huouvcti <74757074+huouvcti@users.noreply.github.com>
2023-07-20 15:36:29 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int score;
+
+  scanf("%d", &score);
+
+  if(score >= 90){
+    printf("A");
+  } else if(score >= 80){
+    printf("B");
+  } else if(score >= 70){
+    printf("C");
+  } else if(score >= 60){
+    printf("D");
+  } else{
+    printf("F");
+  }
+  return 0;
+}
