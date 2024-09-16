4f4ed1068e53331fcc03e0f497832d79f6e70e93
김나경 <74757074+huouvcti@users.noreply.github.com>
2024-09-16 09:22:50 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    while(1){
+        cin >> a >> b;
+        if(cin.eof()) break;
+        cout << a+b << "\n";
+    }
+    return 0;
+}
