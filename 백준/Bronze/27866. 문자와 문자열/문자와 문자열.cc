79ba769096f1647bd3b1755894318c973537acc7
김나경 <74757074+huouvcti@users.noreply.github.com>
2024-09-29 11:06:24 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    int i;
+    cin >> str;
+    cin >> i;
+    
+    cout << str[i-1];
+    return 0;
+}
